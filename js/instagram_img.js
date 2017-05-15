13e1a4d4cc1720ad0ed011298bbf15882ce305d5
Octavian Rotari <octavianrotari@gmail.com>
2017-05-15 17:16:18 +0100

    jshint

--- js/instagram_img.js
+++ js/instagram_img.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-	var img_div = "<div class='photo' id='photo-%img_id%' data-img_url='%img_url%'></div> ";
+	var imgDiv = "<div class='photo' id='photo-%img_id%' data-imgUrl='%img_url%'></div> ";
 
 	jQuery.when(getImgUrlArray()).done(function(data) {
 		addDivImgToHtml(data, setBackgroundImg);
@@ -21,8 +21,8 @@ jQuery(document).ready(function($) {
 	function addDivImgToHtml(data, callback) {
 		for(var i = 0; i < 5; i++) {
 			console.log(data[i]);
-			var final_div = img_div.replace('%img_id%', i).replace('%img_url%', data[i])
-			jQuery('.instagram-photos .show-more').before(final_div);
+			var finalDiv = imgDiv.replace('%img_id%', i).replace('%img_url%', data[i])
+			jQuery('.instagram-photos .show-more').before(finalDiv);
 		};
 		callback();
 	}
@@ -30,7 +30,7 @@ jQuery(document).ready(function($) {
 	function setBackgroundImg() {
 		jQuery('.photo').each(function() {
 			jQuery(this).css({
-				'background-image': 'url(' + this.dataset.img_url + ')',
+				'background-image': 'url(' + this.dataset.imgUrl + ')',
 				'background-size': 'cover'
 			});
 		})
